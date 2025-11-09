de72fa814513652c57c002ec048f50da8bf85f5a
mladennyc <mladennyc@users.noreply.github.com>
2025-11-08 19:45:53 -0500

    Update backend URL for production deployment

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // Note: Installed mobile apps CANNOT use localhost and must use either a public URL or the LAN IP of the backend.
 
-export const BASE_URL = "http://192.168.1.76:8000";
+export const BASE_URL = "https://kasa-backend-x3m3.onrender.com";
 
